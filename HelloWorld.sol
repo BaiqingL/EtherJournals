227436583c0c5beccf882802529134533faeb48c
AndyL <baiqinglyu@gmail.com>
2017-12-15 23:21:08 -0500

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,11 +1,13 @@
 contract HelloWorld{
     uint balance;
     
-    // constructor name has to be same as contract
+    //constructor name has to be same as contract
     //runs once upon contract creation
     //runs on deploy and never again
     function HelloWorld(){
+    //Computation costs gas, while storing values to the blockchain would be more expensive compared to addition and subtraction
         balance = 69;
         
     }
+    //Solidity real time complier
 }
