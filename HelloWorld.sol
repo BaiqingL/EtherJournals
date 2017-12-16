b4203fda7eeff5439f9359243ad08789c1c20803
AndyL <baiqinglyu@gmail.com>
2017-12-15 23:19:52 -0500

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,11 @@
+contract HelloWorld{
+    uint balance;
+    
+    // constructor name has to be same as contract
+    //runs once upon contract creation
+    //runs on deploy and never again
+    function HelloWorld(){
+        balance = 69;
+        
+    }
+}
