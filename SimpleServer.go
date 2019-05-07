671ab37a2640c815a94bbeb0027170ec00aed11b
AndyL <baiqinglyu@gmail.com>
2019-05-06 21:21:24 -0400

    Update SimpleServer.go
    Alright the server dies along with the first connection, first step

--- SimpleServer.go
+++ SimpleServer.go
@@ -8,72 +8,64 @@ import(
 )
 
 
-// Make sure there is no secondary error, panic if otherwise
 func check(err error, message string) {
+
     if err != nil {
         panic(err)
     }
     fmt.Printf("%s\n", message)
+
 }
+
+func handleConnection(conn net.Conn){
+
+  // Close connection when this function ends
+  defer func() {
+    fmt.Println("Closing connection.")
+    conn.Close()
+  }()
+
+  timeoutDuration := 5 * time.Second
+  buf := bufio.NewReader(conn)
+    for{
+      conn.SetReadDeadline(time.Now().Add(timeoutDuration))
+      msg, err := buf.ReadString('\n')
+      fmt.Printf("Data recieved: %v", msg)
+
+      if string(msg) == "close\n"{
+        fmt.Printf("Connection closed by host\n")
+        conn.Write([]byte("goodbye.\n"))
+        conn.Close()
+      }
+
+      if err != nil{
+        fmt.Printf("\nConnection terminated.\n")
+        conn.Close()
+      }
+
+      conn.Write([]byte("Connection recieved\n"))
+    }
+}
+
 
 
 func main() {
 
-  // Begin tcp server
+  defer func() {
+    ln.Close()
+    fmt.Println("Listener closed")
+  }()
+
   ln, err := net.Listen("tcp", ":8080")
   check(err, "Server Ready")
 
-  // Forever loop to accept connections
   for {
 
-    // Start to accept connections
     conn, err := ln.Accept()
 
     check(err, "Connection established")
 
-    // goroutine to process data from client
-    go func(){
-
-      //Read connection
-      buf := bufio.NewReader(conn)
-
-
-      // Keep connection alive
-      for {
-
-        dataRecieved := make(chan bool, 1)
-        defer close(dataRecieved)
-
-        fmt.Println("timer started")
-        timer := time.NewTimer(3 * time.Second)
-        defer timer.Stop()
-
-        go func(){
-          msg, err := buf.ReadString('\n')
-          fmt.Printf("Data recieved: %v", msg)
-          dataRecieved <- true
-
-          // Mind the delim byte!
-          if string(msg) == "close\n"{
-            fmt.Printf("Connection closed by host\n")
-            conn.Write([]byte("goodbye.\n"))
-            conn.Close()
-          }
-
-          // If there is an error, terminate connection
-          if err != nil{
-            fmt.Printf("\nConnection terminated.\n")
-            conn.Close()
-          }
-        }()
-
-        select{
-        case <- dataRecieved:
-          conn.Write([]byte("Connection recieved\n"))
-        case <- timer.C:
-          conn.Close()
-        }
-      }
-    }()
+    go handleConnection(conn)
   }
+
 }
