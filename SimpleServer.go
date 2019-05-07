f43dec79c644ba39c8065d0c360cd4f26e605ebc
AndyL <baiqinglyu@gmail.com>
2019-05-06 20:15:27 -0400

    Create SimpleServer.go

---
+++ SimpleServer.go
@@ -0,0 +1,77 @@
+package main
+
+// TODO: Figure out the timeout feature later
+
+
+import(
+  "net";
+  "fmt";
+  "bufio";
+  //"time";
+)
+
+
+// Make sure there is no secondary error, panic if otherwise
+func check(err error, message string) {
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("%s\n", message)
+}
+
+
+func main() {
+
+  // Begin tcp server
+  ln, err := net.Listen("tcp", ":8080")
+  check(err, "Server Ready")
+
+  // Forever loop to accept connections
+  for {
+
+    // Start to accept connections
+    conn, err := ln.Accept()
+
+    check(err, "Connection established")
+
+    dataRecieved := make(chan bool, 1)
+
+    // goroutine to process data from client
+    go func(){
+
+      //Read connection
+      buf := bufio.NewReader(conn)
+
+
+      // Keep connection alive
+      for {
+        msg, err := buf.ReadString('\n')
+
+        // Mind the delim byte!
+        if string(msg) == "close\n"{
+          conn.Close()
+        }
+
+        fmt.Printf("Data recieved: %v", msg)
+        go func(){
+          dataRecieved <- true
+        }()
+
+        //fmt.Printf("%v\n", <- dataRecieved)
+
+        conn.Write([]byte("Connection recieved\n"))
+
+
+        // If there is an error, terminate connection
+        if err != nil{
+          fmt.Printf("\nConnection terminated.\n")
+          break
+        }
+
+      }
+    }()
+
+
+  }
+
+}
