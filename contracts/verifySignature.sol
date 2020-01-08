d5131e23d0d1f481ea80e97a6d21bf1909f8265b
Baiqing Lyu <baiqinglyu@gmail.com>
2020-01-07 20:38:46 -0500

    Create verifySignature.sol

---
+++ contracts/verifySignature.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.6.1;
+
+contract verifySignature{
+    
+    address owner;
+    
+    constructor() public {
+        owner = msg.sender;    
+    }
+    
+    function ecrecovery(bytes32 hash, bytes memory sig) internal pure returns (address) {
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        
+        if (sig.length != 65) {
+            return address(0);
+        }
+        
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := and(mload(add(sig, 65)), 255)
+        }
+        
+        if (v < 27) {
+            v += 27;
+        }
+        
+        if (v != 27 && v != 28) {
+            return address(0);
+        }
+        
+        return ecrecover(hash, v, r, s);
+    }
+    
+    function ecverify(bytes32 hash, bytes memory sig) public view returns (bool) {
+        return owner == ecrecovery(hash, sig);
+    }
+
+}
