347b9e45db12c4513b63ea1c4efe0c52afee175f
Baiqing Lyu <baiqinglyu@gmail.com>
2020-01-07 21:20:39 -0500

    Added edge cases from OpenZepplin

--- contracts/verifySignature.sol
+++ contracts/verifySignature.sol
@@ -31,11 +31,15 @@ contract verifySignature{
             return address(0);
         }
         
+        if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            return address(0);
+        }
+        
         return ecrecover(hash, v, r, s);
     }
     
     function ecverify(bytes32 hash, bytes memory sig) public view returns (bool) {
         return owner == ecrecovery(hash, sig);
     }
-
+    
 }
